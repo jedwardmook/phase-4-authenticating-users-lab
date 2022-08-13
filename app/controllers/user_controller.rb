8918005241817a2de43bac3ccf841a9f8f13fbc5
JedwardMook <jedwardmook@yahoo.com>
2022-08-13 18:16:08 -0500

    commit tree 45fdff35cf2f57a6ced3e37e3e797db67fd0eb44

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,9 @@
+class UserController < ApplicationController
+
+    def show
+        user = User.find_by(id: session[:user_id])
+        if user
+            render json: user
+        end
+    end
+end
